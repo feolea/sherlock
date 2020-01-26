840064f6d060877171311bae311ffa16c3459d6a
Fernando Olea <fernando.olea76@gmail.com>
2020-01-26 13:14:24 -0200

    adds product model

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Product, type: :model do
+  it { is_expected.to respond_to(:title) }
+  it { is_expected.to respond_to(:description) }
+  it { is_expected.to respond_to(:country) }
+  it { is_expected.to respond_to(:price) }
+  it { is_expected.to respond_to(:created_at) }
+  it { is_expected.to respond_to(:updated_at) }
+end
